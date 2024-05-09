d3ccf2fe7e6c389bff82880440ecc2f339e10ce6
Layan Fernando <158338254+LayanFernando50@users.noreply.github.com>
2024-05-09 18:28:43 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector(".B1-container");
+button.addEventListener("click", () => {
+  console.log("Checkout");
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
